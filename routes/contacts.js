df26fba02b11ae2a0c20073b7b2c62423ef0775c
Cipher715 <atsushi.karino@gmail.com>
2024-10-13 09:19:59 +0900

    Authentication

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,14 +3,12 @@ const router = express.Router();
 
 const userController = require('../controllers/contacts');
 const validation = require('../middleware/validate');
+const {isAuthenticated} = require('../middleware/authenticate');
 
 router.get('/', userController.getAll);
 router.get('/:id', userController.getSingle);
-
-router.post('/', validation.saveContact, userController.createContact);
-
-router.put('/:id', validation.saveContact, userController.updateContact);
-
-router.delete('/:id', userController.deleteContact);
+router.post('/', isAuthenticated, validation.saveContact, userController.createContact);
+router.put('/:id', isAuthenticated, validation.saveContact, userController.updateContact);
+router.delete('/:id', isAuthenticated, userController.deleteContact);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
-    //#swagger.tags=['Home']
-    res.send('Hello World')
-});
+// router.get('/', (req, res) => {
+//     //#swagger.tags=['Home']
+//     res.send('Hello World');
+//     console.log(req.session.user)
+// });
 
 router.use('/contacts', require('./contacts'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {return next(err); }
+        res.redirect('/');
+    });
+});
 
 
 module.exports = router;
